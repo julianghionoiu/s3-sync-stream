20009889481bdc588961e90540219c7d0bd2f3ec
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-04-11 00:02:11 +0100

    Create Gradle project. Write README. Sketch the first acceptance test.

---
+++ src/main/java/tdl/s3/SyncFileApp.java
@@ -0,0 +1,8 @@
+package tdl.s3;
+
+public class SyncFileApp {
+
+    public static void main(String[] args) {
+        System.out.printf("Sync");
+    }
+}
---
+++ src/test/java/tdl/s3/A_OnDemand_FileUpload_AccTest.java
@@ -0,0 +1,27 @@
+package tdl.s3;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class A_OnDemand_FileUpload_AccTest {
+
+    @Test
+    public void should_not_upload_file_if_already_present() throws Exception {
+        //TODO Start by looking at "https://aws.amazon.com/sdk-for-java/". I have already included the libraries in this project.
+
+        //TODO Given-when-then
+    }
+
+    @Ignore("Not implemented")
+    @Test
+    public void should_upload_simple_file_to_bucket() throws Exception {
+
+    }
+
+    @Ignore("Not implemented")
+    @Test
+    public void should_upload_large_file_to_bucket_using_multipart_upload() throws Exception {
+
+    }
+
+}
