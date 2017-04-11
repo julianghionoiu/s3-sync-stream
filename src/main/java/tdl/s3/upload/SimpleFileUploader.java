b7d906b8e453c9f0ebd30fc48d14aec2e3bcc5f2
DrBAXA <DrBAXA@hotmail.com>
2017-04-11 21:10:06 +0300

    SimpleFileUploader done

---
+++ src/main/java/tdl/s3/upload/SimpleFileUploader.java
@@ -0,0 +1,72 @@
+package tdl.s3.upload;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+
+/**
+ * @author vdanyliuk
+ * @version  11.04.17.
+ */
+public class SimpleFileUploader implements FileUploader {
+
+    public static int RETRY_TIMES_COUNT = 2;
+
+    private final AmazonS3 s3Provider;
+    private final String bucket;
+
+    public SimpleFileUploader(final AmazonS3 s3Provider, String bucket) {
+        this.s3Provider = s3Provider;
+        this.bucket = bucket;
+    }
+
+    @Override
+    public void upload(File file) {
+        upload(file, file.getName());
+    }
+
+    @Override
+    public void upload(File file, String newName) {
+        upload(file, newName, RETRY_TIMES_COUNT);
+    }
+
+    private void upload(File file, String newName, int retry) {
+        try {
+            if (!exists(bucket, newName)) {
+                s3Provider.putObject(bucket, newName, file);
+            }
+        } catch (Exception e) {
+            if (retry == 0) {
+                throw new UploadingException("Can't upload file " + file + " due to error " + e.getMessage(), e);
+            } else {
+                upload(file, newName, retry - 1);
+            }
+        }
+    }
+
+    @Override
+    public boolean exists(String bucketName, String fileKey) {
+        ObjectListing objectListing = s3Provider.listObjects(bucketName);
+        return Stream.of(objectListing)
+                .flatMap(this::getNextListing)
+                .map(ObjectListing::getObjectSummaries)
+                .flatMap(Collection::stream)
+                .map(S3ObjectSummary::getKey)
+                .anyMatch(fileKey::equals);
+    }
+
+    private Stream<ObjectListing> getNextListing(ObjectListing objectListing) {
+        if (objectListing.isTruncated()) {
+            return Stream.of(objectListing);
+        } else {
+            ObjectListing nextListing = s3Provider.listNextBatchOfObjects(objectListing);
+            return Stream.concat(Stream.of(objectListing), getNextListing(nextListing));
+        }
+    }
+}
---
+++ src/main/java/tdl/s3/upload/UploadingException.java
@@ -0,0 +1,13 @@
+package tdl.s3.upload;
+
+/**
+ * @author vdanyliuk
+ * @version 11.04.17.
+ */
+public class UploadingException extends RuntimeException {
+
+    public UploadingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
