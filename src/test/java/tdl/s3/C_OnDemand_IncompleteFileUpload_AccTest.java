eec4557700260c31490d6be6542e2dd541d92cef
DrBAXA <DrBAXA@hotmail.com>
2017-04-19 08:59:35 +0300

    Check hashes in test

--- src/test/java/tdl/s3/C_OnDemand_IncompleteFileUpload_AccTest.java
+++ src/test/java/tdl/s3/C_OnDemand_IncompleteFileUpload_AccTest.java
@@ -1,15 +1,25 @@
 package tdl.s3;
 
-import com.amazonaws.services.s3.model.AbortMultipartUploadRequest;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ListMultipartUploadsRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import ch.qos.logback.core.encoder.ByteArrayUtil;
+import com.amazonaws.services.s3.model.*;
+import com.sun.javafx.collections.MappingChange;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.*;
 import org.junit.rules.ExternalResource;
 import org.junit.runners.MethodSorters;
+import sun.misc.IOUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
@@ -28,7 +38,7 @@ public class C_OnDemand_IncompleteFileUpload_AccTest {
     @ClassRule
     public static ExternalResource resource = new ExternalResource() {
         @Override
-        protected void after() {
+        protected void before() {
             fileChecking.getAmazonS3().listMultipartUploads(new ListMultipartUploadsRequest(fileChecking.getBucketName()))
                     .getMultipartUploads()
                     .forEach(upload -> {
@@ -47,16 +57,16 @@ public class C_OnDemand_IncompleteFileUpload_AccTest {
 
     /**
      * NOTES
-     *
+     * <p>
      * This is where it gets interesting. :)
      * We want to start uploading the video recording as soon as it starts being generated.
-     *
+     * <p>
      * I am using https://github.com/julianghionoiu/dev-screen-record to generate the recording and
      * it is configured such that information is always appended to the end of the video and never changed.
      * Internally, the video recording is composed of small independent parts and can be played even if the
      * recording has not stopped.
      * This way we can upload chunks of video as it is being generated.
-     *
+     * <p>
      * The tricky bit is to detect when the recording has completed and the upload can be finalised.
      */
 
@@ -72,12 +82,51 @@ public class C_OnDemand_IncompleteFileUpload_AccTest {
         String[] uploadingArgs = (fileChecking.commonArgs() + "upload -f " + tempFileRule.getFileToUpload()).split(" ");
         SyncFileApp.main(uploadingArgs);
 
+        //create parts hashes
+        Map<Integer, String> hashes = calcHashes(tempFileRule.getFileToUpload());
+
         //Check that the file still not exists on the server
-        ObjectMetadata objectMetadata = fileChecking.getObjectMetadata(tempFileRule.getFileToUpload().getName());
+        String fileName = tempFileRule.getFileToUpload().getName();
+        ObjectMetadata objectMetadata = fileChecking.getObjectMetadata(fileName);
         assertNull(objectMetadata);
         //Check that multipart upload started
-        boolean started = fileChecking.isMultipartUploadExists(tempFileRule.getFileToUpload().getName());
+        boolean started = fileChecking.isMultipartUploadExists(fileName);
         assertTrue(started);
+        //check hash of uploaded parts
+        fileChecking.getAmazonS3()
+                .listMultipartUploads(new ListMultipartUploadsRequest(fileChecking.getBucketName()))
+                .getMultipartUploads().stream()
+                .filter(upload -> upload.getKey().equals(fileName))
+                .map(getMultipartUploadPartListing(fileName))
+                .flatMap(listing -> listing.getParts().stream())
+                .forEach(partSummary -> comparePart(partSummary, hashes));
+    }
+
+    private Map<Integer, String> calcHashes(File fileToUpload) throws IOException, NoSuchAlgorithmException {
+        long fileSize = Files.size(fileToUpload.toPath());
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        Map<Integer, String> result = new HashMap<>(3, 2);
+        try (FileInputStream fileInputStream = new FileInputStream(fileToUpload)) {
+            long read = 0;
+            for (int i = 1; read < fileSize; i++) {
+                int DEFAULT_CHUNK_SIZE = 5 * 1024 * 1024;
+                int chunkSize = fileSize - read > DEFAULT_CHUNK_SIZE ? DEFAULT_CHUNK_SIZE : (int) (fileSize - read);
+                byte[] chunk = IOUtils.readFully(fileInputStream, chunkSize, true);
+                String hash = Hex.encodeHexString(digest.digest(chunk));
+                result.put(i, hash);
+                read += chunk.length;
+            }
+        }
+        return result;
+    }
+
+    private void comparePart(PartSummary partSummary, Map<Integer, String> hashes) {
+        int partNumber = partSummary.getPartNumber();
+        assertEquals(hashes.get(partNumber), partSummary.getETag());
+    }
+
+    private Function<MultipartUpload, PartListing> getMultipartUploadPartListing(String fileName) {
+        return upload -> fileChecking.getAmazonS3().listParts(new ListPartsRequest(fileChecking.getBucketName(), fileName, upload.getUploadId()));
     }
 
 
@@ -85,20 +134,46 @@ public class C_OnDemand_IncompleteFileUpload_AccTest {
     public void b_should_be_able_to_continue_incomplete_file_and_finalise() throws Exception {
 
         //write additional data and delete lock file
-        tempFileRule.writeDataToFile(tempFileRule.getFileToUpload());
+        File fileToUpload = tempFileRule.getFileToUpload();
+        tempFileRule.writeDataToFile(fileToUpload);
         Files.delete(tempFileRule.getLockFile().toPath());
 
         //Start uploading the rest of the file
-        String[] uploadingArgs = (fileChecking.commonArgs() + "upload -f " + tempFileRule.getFileToUpload()).split(" ");
+        String[] uploadingArgs = (fileChecking.commonArgs() + "upload -f " + fileToUpload).split(" ");
         SyncFileApp.main(uploadingArgs);
 
         //Check that the file exists on the server
-        ObjectMetadata objectMetadata = fileChecking.getObjectMetadata(tempFileRule.getFileToUpload().getName());
+        String fileName = fileToUpload.getName();
+        ObjectMetadata objectMetadata = fileChecking.getObjectMetadata(fileName);
         assertNotNull(objectMetadata);
         //Check that multipart upload completed and not exists anymore
-        boolean exists = fileChecking.isMultipartUploadExists(tempFileRule.getFileToUpload().getName());
+        boolean exists = fileChecking.isMultipartUploadExists(fileName);
         assertFalse(exists);
+
+        //check complete file hash. ETag of complete file consists from complete file MD5 hash and some part after "-" sign(probably file version number)
+        assertTrue(objectMetadata.getETag().startsWith(getCompleteFileMD5(fileToUpload)));
 
     }
 
+    /*
+     * Complete file hash in S3 ic calculated as MD5 hash of concatenated MD5 hashes of all parts
+     */
+    private String getCompleteFileMD5(File fileToUpload) throws IOException, NoSuchAlgorithmException {
+        Map<Integer, String> partHashes = calcHashes(fileToUpload);
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        byte[] hashesData = partHashes.keySet().stream()
+                .sorted()
+                .map(partHashes::get)
+                .map(ByteArrayUtil::hexStringToByteArray)
+                .reduce(new byte[0], this::merge);
+        return ByteArrayUtil.toHexString(digest.digest(hashesData));
+    }
+
+    private byte[] merge(byte[] acc, byte[] hash) {
+        int size = acc.length + hash.length;
+        byte[] result = new byte[size];
+        System.arraycopy(acc, 0, result, 0, acc.length);
+        System.arraycopy(hash, 0, result, acc.length, hash.length);
+        return result;
+    }
 }
