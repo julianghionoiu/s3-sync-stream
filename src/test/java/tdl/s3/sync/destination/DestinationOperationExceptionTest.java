5f9b44f088ba47e7a2e10c7e8783afdefe6e4f5a
petra <Petra Barus>
2017-06-03 14:29:01 +0700

    Add test placeholders.

---
+++ src/test/java/tdl/s3/sync/destination/DestinationOperationExceptionTest.java
@@ -0,0 +1,11 @@
+package tdl.s3.sync.destination;
+
+import org.junit.Test;
+
+public class DestinationOperationExceptionTest {
+
+    @Test
+    public void testConstructor() {
+        DestinationOperationException ex = new DestinationOperationException();
+    }
+}
