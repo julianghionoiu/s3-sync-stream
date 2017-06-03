496041f9500bddc8a9269f8bf343a22570e48c15
petra <Petra Barus>
2017-06-03 09:01:56 +0700

    Experiment with Powermock for MD5Digest test.

--- src/main/java/tdl/s3/helpers/MD5Digest.java
+++ src/main/java/tdl/s3/helpers/MD5Digest.java
@@ -8,15 +8,15 @@ public class MD5Digest {
 
     private static MessageDigest md5Digest;
 
-    static {
-        try {
-            md5Digest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Can't send multipart upload. Can't create MD5 digest. " + e.getMessage(), e);
-        }
-    }
-
     public static String digest(byte[] bytes) {
-        return Base64.getEncoder().encodeToString(md5Digest.digest(bytes));
+        if (md5Digest == null) {
+            try {
+                md5Digest = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException("Can't send multipart upload. Can't create MD5 digest. " + e.getMessage(), e);
+            }
+        }
+        byte[] digest = md5Digest.digest(bytes);
+        return Base64.getEncoder().encodeToString(digest);
     }
 }
