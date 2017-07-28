c0ecb47ca53828797455e076034e686f7e82fa09
petra <Petra Barus>
2017-07-28 20:55:27 +0700

    Add atomic counter in progress loader.

--- src/main/java/tdl/s3/sync/progress/UploadStatsProgressListener.java
+++ src/main/java/tdl/s3/sync/progress/UploadStatsProgressListener.java
@@ -3,17 +3,17 @@ package tdl.s3.sync.progress;
 import java.io.File;
 import java.util.Date;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class UploadStatsProgressListener implements ProgressListener {
-
 
     public static class FileUploadStat {
 
         private final double BYTE_PER_MILLISECOND_TO_MEGABYTES_PER_SECOND = 0.001;
 
         private long totalSize = 0;
-
-        private long uploadedSize = 0;
+        
+        private final AtomicLong uploadedSize = new AtomicLong(0);
 
         private long startTimestamp = 0;
 
@@ -27,11 +27,11 @@ public class UploadStatsProgressListener implements ProgressListener {
         }
 
         public long getUploadedSize() {
-            return uploadedSize;
+            return uploadedSize.get();
         }
 
         void incrementUploadedSize(long size) {
-            this.uploadedSize += size;
+            this.uploadedSize.getAndAdd(size);
         }
 
         public double getMBps() {
@@ -39,13 +39,13 @@ public class UploadStatsProgressListener implements ProgressListener {
             if (elapsedMilliseconds == 0) {
                 return 0;
             }
-            double bytesUploaded = (double) this.uploadedSize;
+            double bytesUploaded = (double) this.uploadedSize.get();
             double bytePerMillisecond = bytesUploaded / elapsedMilliseconds;
             return bytePerMillisecond * BYTE_PER_MILLISECOND_TO_MEGABYTES_PER_SECOND;
         }
 
         public double getUploadRatio() {
-            return (double) uploadedSize / (double) totalSize;
+            return (double) uploadedSize.get() / (double) totalSize;
         }
     }
 
---
+++ src/test/java/tdl/s3/sync/progress/FileUploadStatConcurrentTest.java
@@ -0,0 +1,42 @@
+package tdl.s3.sync.progress;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class FileUploadStatConcurrentTest {
+
+    class CounterRunnable implements Runnable {
+
+        private UploadStatsProgressListener.FileUploadStat stat;
+
+        public CounterRunnable(UploadStatsProgressListener.FileUploadStat stat) {
+            this.stat = stat;
+        }
+
+        public void run() {
+            for (int i = 0; i < 1000000; i++) {
+                stat.incrementUploadedSize(1);
+            }
+        }
+    }
+
+    @Test
+    public void incrementUploadSizeInRaceCondition() throws InterruptedException {
+        long total = 1000000 * 2;
+        UploadStatsProgressListener.FileUploadStat stat = new UploadStatsProgressListener.FileUploadStat(total);
+
+        Thread thread1 = new Thread(new CounterRunnable(stat));
+        thread1.setName("add thread");
+        thread1.start();
+
+        Thread thread2 = new Thread(new CounterRunnable(stat));
+        thread2.setName("add thread2");
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        assertEquals(stat.getUploadedSize(), total);
+        assertEquals((double) 1, stat.getUploadRatio(), 0.00001);
+    }
+}
